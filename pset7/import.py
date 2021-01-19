8d3edcb257f0f0c5d06ee40fb76fa56dbb91c745
d2zala <67168290+d2zala@users.noreply.github.com>
2021-01-19 01:10:02 -0700

    Add files via upload

---
+++ pset7/import.py
@@ -0,0 +1,25 @@
+from cs50 import SQL
+from sys import argv, exit
+import csv
+
+if len(argv) != 2:
+    print("Usage: import.py characters.csv")
+    exit(0)
+if argv[1].endswith(".csv") == False:
+    print("Usage: import.py characters.csv")
+    exit(1)
+
+file_name = argv[1]
+#open(file_name, "r").close()
+db = SQL("sqlite:///students.db")
+
+
+with open(file_name, "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        name = row["name"]
+        name_split = name.split()
+        if len(name_split) == 3:
+            db.execute("INSERT INTO students(first, middle, last, house, birth) VALUES (?,?,?,?,?)", name_split[0], name_split[1], name_split[2], row["house"], row["birth"])
+        else:
+            db.execute("INSERT INTO students(first, middle, last, house, birth) VALUES (?,?,?,?,?)", name_split[0], None, name_split[1], row["house"], row["birth"])
